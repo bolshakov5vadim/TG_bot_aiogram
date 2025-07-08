51b647e01256e082ac88bf1505a31413201da307
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2025-07-08 21:15:10 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 from handlers import router
 
-from aiogram import Bot, Dispatcher,types # библиотека Телеграм
+from aiogram import Bot, Dispatcher,types
 from aiogram.fsm.storage.memory import MemoryStorage
+# Библиотека Телеграм
 
 from decouple import Config, RepositoryEnv
 ENV_FILE = 'e.env'
 config = Config(RepositoryEnv(ENV_FILE))
+# Поддержка конфиг-файлов
 
 async def main():
 
- bot = Bot(token=config('TOKEN'))
+ bot = Bot(token=config('TOKEN')) 
 
  commands = [
         types.BotCommand(command='/start', description='Начать взаимодействие с ботом'),
@@ -17,10 +19,11 @@ async def main():
         types.BotCommand(command='/read', description='Чтение данных'),
     ]
  await bot.set_my_commands(commands)
+ # Бот и его подсказки
 
- dp = Dispatcher(storage=MemoryStorage()) # объект обработки сообщений
+ dp = Dispatcher(storage=MemoryStorage()) # Объект обработки сообщений
 
- #основной процесс
+ # Основной процесс
  dp.include_router(router)
  await bot.delete_webhook(drop_pending_updates=True)
  await dp.start_polling(bot)
