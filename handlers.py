baa78cff4ff44ae7e36ccf94d0ce0aa69b5e5218
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2025-07-08 20:59:53 +0500

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -15,32 +15,34 @@ router = Router()
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer(
-"Запуск сообщения по команде /start. \n Commands: create <IMYA> <FAMILIA> \n read <ID> \n update <ID> <IMYA> <FAMILIA>"
+"Запуск сообщения по команде /start. \n Commands: /create <IMYA> <FAMILIA> \n /read <ID> \n /update <ID> <IMYA> <FAMILIA>"
 )
 
 @router.message(Command('create'))
 async def cmd_create(message: Message):
     words, params = message.text.split(" "), ""
-    if (len(words)>1): params='{"name": '+words[1]+', "surname": }'
-    if (len(words)>2): params='{"name": '+words[1]+', "surname": '+words[2]+'}'
-    session = aiohttp.ClientSession()
-    response = session.post(config('API_ADDR'),params)
-    data = await response.json()
-    await message.answer(f"{data}")
+    if (len(words)>1): params={"name": words[1], "surname": None}
+    if (len(words)>2): params={"name": words[1], "surname": words[2]}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(config('API_ADDR'),params) as response:
+            await message.answer(await response.json())
 
 @router.message(F.text == 'read')#F - более гибкая
 async def cmd_read(message: Message):
-    session = aiohttp.ClientSession()
-    response = session.get(config('API_ADDR')+message.text.split(" ")[1])
-    data = await response.json()
-    await message.answer(f"{data}")
+    words, params = message.text.split(" "), ""
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(config('API_ADDR')+'/'+words[1]) as response:
+            await message.answer(await response.json())
 
 @router.message(F.text == 'update')#F - более гибкая
 async def cmd_update(message: Message):
     words, params = message.text.split(" "), ""
-    if (len(words)>1): params='{"name": '+words[1]+', "surname": }'
-    if (len(words)>2): params='{"name": '+words[1]+', "surname": '+words[2]+'}'
-    session = aiohttp.ClientSession()
-    response = session.put(config('API_ADDR'), params)
-    data = await response.json()
-    await message.answer(f"{data}")
+    if (len(words)>1): params={"name": words[1], "surname": None}
+    if (len(words)>2): params={"name": words[1], "surname": words[2]}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.put(config('API_ADDR'), params) as response:
+            await message.answer(await response.json())
+
