66ba4aa260a951659c71fac8be3579d9cd8c1574
bolshakov5vadim <99659244+bolshakov5vadim@users.noreply.github.com>
2025-07-08 20:15:05 +0500

    handlers.py

--- handlers.py
+++ handlers.py
@@ -20,7 +20,7 @@ async def cmd_start(message: Message):
 
 @router.message(Command('create'))
 async def cmd_create(message: Message):
-    words = message.text.split(" ")
+    words, params = message.text.split(" "), ""
     if (len(words)>1): params='{"name": '+words[1]+', "surname": }'
     if (len(words)>2): params='{"name": '+words[1]+', "surname": '+words[2]+'}'
     session = aiohttp.ClientSession()
